9ee7a076f5211d81c2cf1c8a73c8cf58b72ca002
AntonyJamesGitHub <aj339_07@hotmail.co.uk>
2021-09-20 15:02:35 +0100

    First draft.

--- Interview/Controllers/ValuesController.cs
+++ Interview/Controllers/ValuesController.cs
@@ -1,39 +1,78 @@
-﻿using System;
+﻿using Interview.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Interview.Controllers
 {
     public class ValuesController : ApiController
     {
+        //Used Google Chrome extension "ReqBin HTTP Client" to test
+        
         // GET api/values
-        public IEnumerable<string> Get()
+        public List<Transaction> Get()
         {
-            return new string[] { "value1", "value2" };
+            return GetTransactions();
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public Transaction Get(string id)
         {
-            return "value";
+            return GetTransactions().Where(t => t.Id == id).FirstOrDefault(); //TODO: do something if FirstOrDefault returns null?
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Transaction value)
         {
+            //TODO: avoid reading and rewriting entire file as explained here - https://stackoverflow.com/questions/43529386/append-data-in-a-json-file-in-c-sharp/43529523
+
+            var transactionList = GetTransactions();
+            transactionList.Add(value);
+            string json = JsonConvert.SerializeObject(transactionList);
+            File.WriteAllText(@"C:\VisualStudioProjects\Test\data.json", json);
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(string id, [FromBody]Transaction value)
         {
+            var transactionList = GetTransactions();
+
+            //Opted for foreach instead of linq and lambda as it looks cleaner on this occasion
+            foreach (var t in transactionList.Where(t => t.Id == value.Id))
+            {
+                t.ApplicationId = value.ApplicationId;
+                t.Type = value.Type;
+                t.Summary = value.Summary;
+                t.Amount = value.Amount;
+                t.PostingDate = value.PostingDate;
+                t.IsCleared = value.IsCleared;
+                t.ClearedDate = value.ClearedDate;
+            }
+            string json = JsonConvert.SerializeObject(transactionList);
+            File.WriteAllText(@"C:\VisualStudioProjects\Test\data.json", json);
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public void Delete(string id)
         {
+            var transactionList = GetTransactions();
+            var transaction = transactionList.Find(t => t.Id == id);
+            transactionList.Remove(transaction);
+            string json = JsonConvert.SerializeObject(transactionList);
+            File.WriteAllText(@"C:\VisualStudioProjects\Test\data.json", json);
+        }
+
+        private List<Transaction> GetTransactions()
+        {
+            string json = File.ReadAllText(@"C:\VisualStudioProjects\Test\data.json");
+            var transactionList = JsonConvert.DeserializeObject<List<Transaction>>(json);
+            return transactionList;
         }
     }
 }
---
+++ Interview/Models/Transaction.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Interview.Models
+{
+    //TODO: move enums elsewhere
+    //TODO: consider changing Id from string to Guid
+    
+    public enum TransactionType
+    {
+        [EnumMember(Value = "Debit")]
+        Debit,
+        [EnumMember(Value = "Credit")]
+        Credit
+    }
+
+    public enum TransactionSummary
+    {
+        [EnumMember(Value = "Payment")]
+        Payment,
+        [EnumMember(Value = "Refund")]
+        Refund
+    }
+
+    public class Transaction
+    {
+        public string Id { get; set; }
+        public int ApplicationId { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TransactionType Type { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TransactionSummary Summary { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PostingDate { get; set; }
+        public bool IsCleared { get; set; }
+        public DateTime? ClearedDate { get; set; }
+    }
+}
